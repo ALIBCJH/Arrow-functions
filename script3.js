d6bed415de160e9f5c72f98ff8eec998b23d8c16
simonjuma_230 <simon.ombom22@students.dkut.ac.ke>
2024-03-26 16:19:22 +0300

    first commit

---
+++ script3.js
@@ -0,0 +1,71 @@
+function calcAddition(number1, number2){
+  return number1 + nummber2;
+}
+calcAddition()
+console.log(calcAddition(6,9));
+/* In the example code above, we have created a function named calcAddition
+that takes two numbers as the parameter and returns the addition of the 2 numbers
+*/
+
+//ARROW FUNCTION 
+const a = ["Hydrogen","Helium", "Lithium", "Berylium"];
+
+const a2 = a.map(function (s){
+  return s.length;
+});
+console.log("Normal way", a2);
+
+const a3 = a.map((s) => s.length);
+console.log("Using Arrow Function", a3);
+//ARROW FUNCTION 
+const a = ["Hydrogen","Helium", "Lithium", "Berylium"];
+
+const a2 = a.map(function (s){
+  return s.length;
+});
+console.log("Normal way", a2);
+
+const a3 = a.map((s) => s.length);
+console.log("Using Arrow Function", a3);
+//Parameter are additional information passed to a functio
+
+function multiply(a,b){
+    b = typeof b !== "undefined" ? b:1;
+    return a*b;
+    
+}
+console.log(multiply(69));
+// Function Call
+
+
+function welcomeMsg(name){
+  return ("Hello" + name + "welcome to GeeksforGeeks");
+  
+}
+let nameVal = "Admin"
+
+console.log(welcomeMsg(nameVal));
+let function_name = argument1 => expression /*this is one is for passing single arguments */
+ 
+let function_name = (argument1, argument2,) => expression
+
+//Tradditional way of defining java script functions
+ function multiply(a,b){
+     return a * b;
+ }
+ console.log(multiply(3,5));
+ 
+ //Defining Java script functions using arrow function
+ value = (a,b) => ;
+ console.log(value(3,5)); // Arrow functions are more popular among developers beacuse they are succintness
+ //Arrow functions with no parameters
+ const string = () => "geeksforgeeks";
+ console.log(string);
+ 
+ //NESTED FUNCTIONS IN  ARROW FUNCTIONS
+ let array = ['sam', 'serah', 'john'];
+ 
+ let lengths = array.map((string) => string.length);
+ 
+ console.log(lengths);
+ //output = [3,4,5]
